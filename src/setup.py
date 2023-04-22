70483a6a3649f427d5564dd040938999866e7bf3
Jared Bennett <jrdbnntt@gmail.com>
2023-04-22 09:30:29 -0700

    setup.py

---
+++ src/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='reserve_canadian_rockies',
+    version='1.0.0',
+    description='',
+    long_description='',
+    author='Jared Bennett',
+    author_email='',
+    license='MIT',
+    packages=[
+        'reserve_canadian_rockies'
+    ],
+    zip_safe=False,
+)
